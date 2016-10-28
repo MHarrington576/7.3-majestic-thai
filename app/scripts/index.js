78b609cc22adfab36cb8f38e41f7231228c3fbc7
Matthew Harrington <mattharrington576@gmail.com>
2016-10-28 15:49:41 -0400

    Formatting homepage

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,2 +1,7 @@
+var $ = require('jquery');
 var Backbone = require('backbone');
-var React = require('react');
+require('./router');
+
+$(function(){
+  Backbone.history.start();
+});
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -4,31 +4,48 @@ var ReactDOM = require('react-dom');
 
 var AppRouter = Backbone.Router.extend({
   routes: {
-    '': 'index',
+    '': 'home',
     'menu/': 'menu',
-    'staff/': 'staff'
-  }
+    'about/': 'about',
+    'order/': 'order',
+    'kitchen/': 'kitchen'
+  },
 
-  index: function(){
+  home: function(){
     ReactDOM.render(
       React.createElement({router: this}),
-      document.getElementById('app');
+      document.getElementById('app')
     );
   },
 
   menu: function(){
     ReactDOM.render({
       React.createElement({router: this}),
-      document.getElementById('app');
-    });
-  },
-
-  staff: function(){
-    ReactDOM.render(
-      React.createElement({router: this}),
-      document.getElementById('app');
+      document.getElementById('app')
     );
   },
+
+  order: function(){
+    ReactDOM.render(
+      React.createElement({router: this}),
+      document.getElementById('app')
+    );
+  },
+
+  about: function(){
+    ReactDOM.render(
+      React.createElement({router: this}),
+      document.getElementById('app')
+    );
+  },
+
+  kitchen: function(){
+// Authenticate with password?
+    ReactDOM.render(
+      React.createElement({router: this}),
+      document.getElementById('app')
+    );
+  }
 });
 
 var router = new AppRouter();
