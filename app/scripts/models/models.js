cd7e9e5f0042c21ef1f3b4e414778510f11e57e7
Matthew Harrington <mattharrington576@gmail.com>
2016-10-29 00:54:09 -0400

    Added models

--- app/scripts/models/models.js
+++ app/scripts/models/models.js
@@ -1,4 +1,22 @@
 var Backbone = require('backbone');
 var React = require('react');
 
-var MenuItem = Backbone.Model.extend();
+var MenuItem = Backbone.Model.extend({
+  defaults: {
+    thaiName: '',
+    englishName: '',
+    description: '',
+    imageUrl: '',
+    price: 0
+  }
+});
+
+var MenuCollection = Backbone.Collection.extend({
+  model: MenuItem,
+  url: '...'
+});
+
+module.exports = {
+  MenuItem: MenuItem,
+  MenuCollection: MenuCollection
+}
